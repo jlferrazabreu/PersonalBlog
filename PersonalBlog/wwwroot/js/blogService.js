45084f576946b8814733689119e1aa865ba8ddba
Jorge Luis Ferraz de Abreu <jlferrazabreu@gmail.com>
2022-03-28 17:31:27 -0300

    Aula-04

--- PersonalBlog/wwwroot/js/blogService.js
+++ PersonalBlog/wwwroot/js/blogService.js
@@ -51,7 +51,8 @@
             template.showBlogItem(html, link);
             window.location = '#' + link;
         } else {
-            template.showBlogItem($('#blog-content-not-found').html(), link);
+            let blogItemHtml = $('#blog-content-not-found').html().replace(/{{Link}}/g, link);
+            template.showBlogItem(blogItemHtml, link);
             window.location = '#blog-item-container';
         }
     }
